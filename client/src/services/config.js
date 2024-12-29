60d4ae024c107a7f47674b70e812594a94b01b96
Manish <mdashsharma95@gmail.com>
2024-12-29 18:58:08 +0530

    feat: Github action integrated

--- client/src/services/config.js
+++ client/src/services/config.js
@@ -2,8 +2,6 @@ import axios from 'axios';
 
 const baseURL = import.meta.env.VITE_SERVER_URL;
 
-// const baseURL = 'http://localhost:5000'
-
 if (!baseURL) {
     throw new Error('VITE_SERVER_URL is not defined in the environment variables.');
 }
