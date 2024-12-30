7bbc34a896ac067e9a82924ba5923c5513089a1f
Manish <mdashsharma95@gmail.com>
2024-12-30 17:06:49 +0530

    fix: Chnages in Server API route

--- client/src/services/api.services.js
+++ client/src/services/api.services.js
@@ -2,16 +2,16 @@ import { servicesAxiosInstance } from './config';
 
 
 export const getServerHealth = async () => {
-  const response = await servicesAxiosInstance.get('/api/v1/health');
+  const response = await servicesAxiosInstance.get('/v1/health');
   return response.data;
 };
 
 export const saveVisitorLocation = async () => {
-  const response = await servicesAxiosInstance.get('/api/v1/save-location-stats');
+  const response = await servicesAxiosInstance.get('/v1/save-location-stats');
   return response.data
 }
 
 export const apiDetailsStatus = async() =>{
-  const response = await servicesAxiosInstance('/api/v1/api-details-check')
+  const response = await servicesAxiosInstance('/v1/api-details-check')
   return response.data
 }
