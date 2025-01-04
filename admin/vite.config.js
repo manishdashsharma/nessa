ccc21fc9a5f5491cdca31545db24d1faa40fc629
Manish <mdashsharma95@gmail.com>
2025-01-04 18:59:28 +0530

    fix: Production fixes

--- admin/vite.config.js
+++ admin/vite.config.js
@@ -1,15 +1,14 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:'/admin/',
+  base: '/admin/',
   server: {
-    host: true, 
-    port: 8000, 
+    host: true,
+    port: 8000,
     watch: {
-      usePolling: true, 
+      usePolling: true,
     },
   },
 });
