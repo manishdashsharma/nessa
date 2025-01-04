e7734a7535e60f42e4342840eaf9c5611d890227
Manish <mdashsharma95@gmail.com>
2025-01-04 18:44:14 +0530

    fix: Vite config

--- admin/vite.config.js
+++ admin/vite.config.js
@@ -4,10 +4,10 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base:'/admin/',
   server: {
     host: true, 
     port: 8000, 
-    base:'/admin/',
     watch: {
       usePolling: true, 
     },
