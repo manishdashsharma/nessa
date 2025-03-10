147db7676aa2ed76eb258e05240909d8d2f35966
piyush-github12 <piyushgithubaccouunt@gmail.com>
2025-03-11 01:10:18 +0530

    fix: Baseurl fix

--- client/src/services/config.js
+++ client/src/services/config.js
@@ -4,12 +4,12 @@ const baseURL = import.meta.env.VITE_SERVER_URL;
 
 
 
-// if (!baseURL) {
-//     throw new Error('VITE_SERVER_URL is not defined in the environment variables.');
-// }
+if (!baseURL) {
+    throw new Error('VITE_SERVER_URL is not defined in the environment variables.');
+}
 
 const servicesAxiosInstance = axios.create({
-    baseURL: 'http://localhost:5000' //piyush remember to fix this before pushing
+    baseURL: baseURL 
 });
 
 export {
